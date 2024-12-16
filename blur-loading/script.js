9d85ba5109176e9f5feb31771fc89286d4f92b9c
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-12-16 11:52:27 +0700

    blurry loading for background image

---
+++ blur-loading/script.js
@@ -0,0 +1,29 @@
+const bg = document.querySelector(".bg");
+const loadingText = document.querySelector(".loading-text");
+
+// initialize the loading value from 0
+let load = 0;
+// run blurring function every 30ms using setInterval
+let int = setInterval(blurring, 30);
+
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(int);
+  }
+
+  // loading text from 0 to 100
+  loadingText.innerText = `${load}%`;
+  // loading text opacity from 1 to 0 (slowly disappear)
+  loadingText.style.opacity = scale(load, 0, 100, 1, 0);
+  // background blur effect from 30 to 0
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+
+  console.log(load);
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (number, inMin, inMax, outMin, outMax) => {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+};
