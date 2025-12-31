7ae6d7df20723bca00d2daaf8cdf9e3d01163b5e
Nurul Insani Dewi <nurulinsanid@gmail.com>
2025-12-31 23:44:36 +0700

    feat: add interactive info for expanding cards

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -1,4 +1,17 @@
 const panels = document.querySelectorAll(".panel"); // use querySelectorAll to select all panels
+const infoBtn = document.querySelector(".info-btn");
+const infoDesc = document.querySelector(".info-desc");
+
+infoBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  infoDesc.classList.toggle("show");
+});
+
+document.addEventListener("click", (e) => {
+  if (!infoBtn.contains(e.target) && !infoDesc.contains(e.target)) {
+    infoDesc.classList.remove("show");
+  }
+});
 
 // console.log(panels[0]) // targeting specific panel
 
